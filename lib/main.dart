0d040661eec4cffa0a6943a7647c949334be6248
Reza Anwar <ranwar@gmail.com>
2023-01-04 17:30:43 +0700

    Running version

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,321 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const MaterialApp(home: HomePage()));
 
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static Future<bool> getPermissions() async {
+    if (Platform.isIOS) return true;
+    await Permission.camera.request();
+    await Permission.microphone.request();
+    await Permission.bluetoothConnect.request();
+    await FlutterBluetoothSerial.instance.requestEnable();
+
+    while ((await Permission.camera.isDenied)) {
+      await Permission.camera.request();
+    }
+    while ((await Permission.microphone.isDenied)) {
+      await Permission.microphone.request();
+    }
+    while ((await Permission.bluetoothConnect.isDenied)) {
+      await Permission.bluetoothConnect.request();
+    }
+    return true;
+  }
+
+// UI to render join screen
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            // Function to push to meeting page
+            onPressed: () async {
+              await getPermissions();
+              Navigator.push(
+                context,
+                CupertinoPageRoute(builder: (_) => const MeetingPage()),
+              );
+            },
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Text(
+                'Join',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MeetingPage extends StatefulWidget {
+  const MeetingPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MeetingPage> createState() => _MeetingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MeetingPageState extends State<MeetingPage>
+    implements HMSUpdateListener {
+  //SDK
+  late HMSSDK hmsSDK;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // Variables required for joining a room
+  String authToken =
+      "YOUR_TOKEN_HERE";
+  String userName = "test_user";
+
+  // Variables required for rendering video and peer info
+  HMSPeer? localPeer, remotePeer;
+  HMSVideoTrack? localPeerVideoTrack, remotePeerVideoTrack;
+
+  // Initialize variables and join room
+  @override
+  void initState() {
+    super.initState();
+    hmsSDK = HMSSDK();
+    hmsSDK.build();
+    hmsSDK.addUpdateListener(listener: this);
+    hmsSDK.join(config: HMSConfig(authToken: authToken, userName: userName));
+  }
+
+  // Clear all variables
+  @override
+  void dispose() {
+    remotePeer = null;
+    remotePeerVideoTrack = null;
+    localPeer = null;
+    localPeerVideoTrack = null;
+    super.dispose();
+  }
+
+  // Called when peer joined the room - get current state of room by using HMSRoom obj
+  @override
+  void onJoin({required HMSRoom room}) {
+    room.peers?.forEach((peer) {
+      if (peer.isLocal) {
+        localPeer = peer;
+        if (peer.videoTrack != null) {
+          localPeerVideoTrack = peer.videoTrack;
+        }
+        if (mounted) {
+          setState(() {});
+        }
+      }
     });
   }
 
+  // Called when there's a peer update - use to update local & remote peer variables
+  @override
+  void onPeerUpdate({required HMSPeer peer, required HMSPeerUpdate update}) {
+    switch (update) {
+      case HMSPeerUpdate.peerJoined:
+        if (!peer.isLocal) {
+          if (mounted) {
+            setState(() {
+              remotePeer = peer;
+            });
+          }
+        }
+        break;
+      case HMSPeerUpdate.peerLeft:
+        if (!peer.isLocal) {
+          if (mounted) {
+            setState(() {
+              remotePeer = null;
+            });
+          }
+        }
+        break;
+      case HMSPeerUpdate.networkQualityUpdated:
+        return;
+      default:
+        if (mounted) {
+          setState(() {
+            localPeer = null;
+          });
+        }
+    }
+  }
+
+  // Called when there's a track update - use to update local & remote track variables
+  @override
+  void onTrackUpdate(
+      {required HMSTrack track,
+      required HMSTrackUpdate trackUpdate,
+      required HMSPeer peer}) {
+    if (track.kind == HMSTrackKind.kHMSTrackKindVideo) {
+      switch (trackUpdate) {
+        case HMSTrackUpdate.trackRemoved:
+          if (mounted) {
+            setState(() {
+              peer.isLocal
+                  ? localPeerVideoTrack = null
+                  : remotePeerVideoTrack = null;
+            });
+          }
+          return;
+        default:
+          if (mounted) {
+            setState(() {
+              peer.isLocal
+                  ? localPeerVideoTrack = track as HMSVideoTrack
+                  : remotePeerVideoTrack = track as HMSVideoTrack;
+            });
+          }
+      }
+    }
+  }
+
+  // More callbacks - no need to implement for quickstart
+  @override
+  void onAudioDeviceChanged(
+      {HMSAudioDevice? currentAudioDevice,
+      List<HMSAudioDevice>? availableAudioDevice}) {}
+
+  @override
+  void onChangeTrackStateRequest(
+      {required HMSTrackChangeRequest hmsTrackChangeRequest}) {}
+
+  @override
+  void onHMSError({required HMSException error}) {}
+
+  @override
+  void onMessage({required HMSMessage message}) {}
+
+  @override
+  void onReconnected() {}
+
+  @override
+  void onReconnecting() {}
+
+  @override
+  void onRemovedFromRoom(
+      {required HMSPeerRemovedFromPeer hmsPeerRemovedFromPeer}) {}
+
+  @override
+  void onRoleChangeRequest({required HMSRoleChangeRequest roleChangeRequest}) {}
+
+  @override
+  void onRoomUpdate({required HMSRoom room, required HMSRoomUpdate update}) {}
+
+  @override
+  void onUpdateSpeakers({required List<HMSSpeaker> updateSpeakers}) {}
+
+  // Widget to render a single video tile
+  Widget peerTile(Key key, HMSVideoTrack? videoTrack, HMSPeer? peer) {
+    return Container(
+      key: key,
+      child: (videoTrack != null && !(videoTrack.isMute))
+          // Actual widget to render video
+          ? HMSVideoView(
+              track: videoTrack,
+            )
+          : Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue.withAlpha(4),
+                  shape: BoxShape.circle,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 20.0,
+                      spreadRadius: 5.0,
+                    ),
+                  ],
+                ),
+                child: Text(
+                  peer?.name.substring(0, 1) ?? "D",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+    );
+  }
+
+  // Widget to render grid of peer tiles and a end button
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return WillPopScope(
+      // Used to call "leave room" upon clicking back button [in android]
+      onWillPop: () async {
+        hmsSDK.leave();
+        Navigator.pop(context);
+        return true;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(
+            children: [
+              // Grid of peer tiles
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisExtent: (remotePeerVideoTrack == null)
+                          ? MediaQuery.of(context).size.height
+                          : MediaQuery.of(context).size.height / 2,
+                      crossAxisCount: 1),
+                  children: [
+                    if (remotePeerVideoTrack != null && remotePeer != null)
+                      peerTile(
+                          Key(remotePeerVideoTrack?.trackId ?? "" "mainVideo"),
+                          remotePeerVideoTrack,
+                          remotePeer),
+                    peerTile(
+                        Key(localPeerVideoTrack?.trackId ?? "" "mainVideo"),
+                        localPeerVideoTrack,
+                        localPeer)
+                  ],
+                ),
+              ),
+              // End button to leave the room
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    hmsSDK.leave();
+                    Navigator.pop(context);
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.red,
+                  padding: const EdgeInsets.all(15.0),
+                  shape: const CircleBorder(),
+                  child: const Icon(
+                    Icons.call_end,
+                    size: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
